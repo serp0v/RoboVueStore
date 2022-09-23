f777eacaee5ab926184319038738c196b8587408
serp0v <serpov.cc@mail.ru>
2022-09-23 23:24:49 +1000

    add header component (my first!)

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,6 @@
 <template>
+    <robo-header />
     <div class="wrapper">
-
-        <header class="header-wrapper">
-            <div class="header-container">
-                <div class="logo"><p>RoboVue</p></div>
-                <ul class="spa-navigation">
-                    <li class="nav-item">Home</li>
-                    <li class="nav-item">Robots</li>
-                    <li class="nav-item">Software</li>
-                    <li class="nav-item">Contact</li>
-                </ul>
-                <div class="log-sign">
-                    <button class="log-in">
-                        <p>Log in</p>
-                    </button>
-                    <button class="sign-up">
-                        <p>Sign up</p>
-                    </button>
-                </div>
-            </div>
-            <div class="header-container-mobile">
-                <div class="logo"><p>RoboVue</p></div>
-                <div class="burger-menu">
-                    <svg width="33" height="25" viewBox="0 0 33 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <line y1="0.522476" x2="33" y2="0.522476" stroke="white"/>
-                        <line y1="12.1404" x2="33" y2="12.1404" stroke="white"/>
-                        <line y1="23.7584" x2="33" y2="23.7584" stroke="white"/>
-                    </svg>
-                </div>
-            </div>
-        </header>
-
         <main class="main-wrapper">
             <div class="main-container">
                 <div class="company-name">RoboVue</div>
@@ -48,11 +18,14 @@
 </template>
 
 <script>
-// export default {
-//     setup() {
-        
-//     },
-// }
+export default {
+    // setup() {
+    // },
+    components: {
+        RoboHeader
+    }
+}
+    import RoboHeader from "./components/RoboHeader.vue";
 </script>
 
 <style>
@@ -73,72 +46,6 @@
         width: 100vw;
         display: flex;
         flex-direction: column;
-    }
-    /* header-wrapper */
-    
-    .header-wrapper {
-        width: 100%;
-        height: 8.703%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .header-container {
-        max-width: 1102px;
-        margin: 0 auto;
-        height: 60px;
-        display: flex;
-        flex-direction: row;
-        background-color: rgb(255, 255, 255);
-
-    }
-    .logo {
-        display: flex;
-        align-items: center;
-        font-size: 36px;
-        border: 1px solid #000;
-        padding-left: 7px;
-        padding-right: 7px;
-        height: 60px;
-    }
-    .spa-navigation {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        padding-left: 81px;
-        padding-right: 81px;
-    }
-    .nav-item {
-        list-style: none;
-        font-size: 36px;
-    }
-    .nav-item ~ .nav-item {
-        margin-left: 57px;
-    }
-    .log-sign {
-        display:flex;
-        align-items: center;
-        flex-direction: row;
-        font-size:36px;
-    }
-    .log-sign > button {
-        display: flex;
-        font-size: 36px;
-        padding-left: 7px;
-        padding-right: 7px;
-        height: 58px;
-        border: 1px solid #000000;
-        justify-content: center;
-        align-items: center;
-    }
-    button > p {
-        text-align: center;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .log-sign > button ~ button {
-        margin-left: 43px;
     }
     .main-wrapper {
         height: 82.5925vh;
@@ -208,10 +115,6 @@
         .header-container {
             background-color: rgb(0, 0, 0);
             display: none;
-        }
-        .footer-wrapper {
-        }
-        .footer-container {
         }
         .header-container-mobile {
             display: flex;
---
+++ src/components/RoboHeader.vue
@@ -0,0 +1,101 @@
+<template>
+    <header class="header-wrapper">
+        <div class="header-container">
+            <div class="logo"><p>RoboVue</p></div>
+            <ul class="spa-navigation">
+                <li class="nav-item">Home</li>
+                <li class="nav-item">Robots</li>
+                <li class="nav-item">Software</li>
+                <li class="nav-item">Contact</li>
+            </ul>
+            <div class="log-sign">
+                <button class="log-in">
+                    <p>Log in</p>
+                </button>
+                <button class="sign-up">
+                    <p>Sign up</p>
+                </button>
+            </div>
+        </div>
+        <div class="header-container-mobile">
+            <div class="logo"><p>RoboVue</p></div>
+            <div class="burger-menu">
+                <svg width="33" height="25" viewBox="0 0 33 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line y1="0.522476" x2="33" y2="0.522476" stroke="white"/>
+                    <line y1="12.1404" x2="33" y2="12.1404" stroke="white"/>
+                    <line y1="23.7584" x2="33" y2="23.7584" stroke="white"/>
+                </svg>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+</script>
+
+<style>
+    .header-wrapper {
+        width: 100vw;
+        height: 8.703vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .header-container {
+        max-width: 1102px;
+        margin: 0 auto;
+        height: 60px;
+        display: flex;
+        flex-direction: row;
+        background-color: rgb(255, 255, 255);
+
+    }
+    .logo {
+        display: flex;
+        align-items: center;
+        font-size: 36px;
+        border: 1px solid #000;
+        padding-left: 7px;
+        padding-right: 7px;
+        height: 60px;
+    }
+    .spa-navigation {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding-left: 81px;
+        padding-right: 81px;
+    }
+    .nav-item {
+        list-style: none;
+        font-size: 36px;
+    }
+    .nav-item ~ .nav-item {
+        margin-left: 57px;
+    }
+    .log-sign {
+        display:flex;
+        align-items: center;
+        flex-direction: row;
+        font-size:36px;
+    }
+    .log-sign > button {
+        display: flex;
+        font-size: 36px;
+        padding-left: 7px;
+        padding-right: 7px;
+        height: 58px;
+        border: 1px solid #000000;
+        justify-content: center;
+        align-items: center;
+    }
+    button > p {
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .log-sign > button ~ button {
+        margin-left: 43px;
+    }
+</style>
